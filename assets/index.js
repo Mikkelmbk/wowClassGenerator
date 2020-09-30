43dce9ccbda6e74d6c865d8a60985ff7139e365b
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-09-30 13:45:27 +0200

    raceCollection objects ready

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
+// Declarations
 let bodyElem = document.querySelector('body');
 let navElem = document.querySelector('nav');
 let allianceBtnElem = document.querySelector('#allianceBtn');
@@ -5,8 +6,23 @@ let randomBtnElem = document.querySelector('#randomBtn');
 let hordeBtnElem = document.querySelector('#hordeBtn');
 let factionArray = ["Horde", "Alliance"];
 let chosenFaction;
+let raceCollection = {
+    allianceRaces: {
+        human: ["Mage","Paladin","Priest","Rogue","Warlock","Warrior"],
+        nightElf: ["Druid","Hunter","Priest","Rogue","Warrior"],
+        dwarf: ["Hunter","Paladin","Priest","Rogue","Warrior"],
+        gnome: ["Mage","Rogue","Warlock","Warrior"]
+    },
+    hordeRaces: {
+        orc: ["Hunter","Rogue","Shaman","Warlock","Warrior"],
+        undead: ["Mage","Priest","Rogue","Warlock","Warrior"],
+        tauren: ["Druid","Hunter","Shaman","Warrior"],
+        troll: ["Hunter","Mage","Priest","Rogue","Shaman","Warrior"]
+    }
+};
 
 
+// EventListeners
 allianceBtnElem.addEventListener('click', () => {
     chosenFaction = factionArray[1];
     factionHandler(chosenFaction);
@@ -20,16 +36,17 @@ hordeBtnElem.addEventListener('click', () => {
 randomBtnElem.addEventListener('click', () => {
     chosenFaction = factionArray[getRandomNumber(factionArray.length)];
     factionHandler(chosenFaction);
-})
+});
 
 
+// Functions
 function factionHandler(faction) {
     if(faction == "Horde" ? bodyElem.style.backgroundImage = `url("assets/img/horde-banner.jpg")` : bodyElem.style.backgroundImage = `url("assets/img/alliance-banner.jpg")`);
     bodyElem.style.backgroundPosition = "center";
     bodyElem.style.backgroundSize = "cover";
     bodyElem.style.backgroundRepeat = "no-repeat";
-}
+};
 
 function getRandomNumber(number) {
     return Math.floor(Math.random() * number);
-}
+};
